eb9477969bc7f829991dba8a9615e7a4c4a91d04
Livia Degrossi <liviadegrossi@gmail.com>
2026-04-02 08:14:51 +0200

    Start school app and create a serializer

---
+++ school/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from school.models import Student, Course
+
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'cpf', 'date_birth', 'phone_number')
+    list_display_links = ('id', 'name', )
+    list_per_page = 20
+    search_fields = ('name', )
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description')
+    list_display_links = ('id', 'code', )
+    search_fields = ('code', )
+
+admin.site.register(Course, Courses)
---
+++ school/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=60, blank=False)
+    cpf = models.CharField(max_length=11)
+    date_birth = models.DateField()
+    phone_number = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    LEVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    code = models.CharField(max_length=10)
+    description = models.TextField(blank=False)
+    level = models.CharField(max_length=1, choices=LEVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.code
